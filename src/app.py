4e731f474d32aa6d803ed3e6edccd55f3d67698b
sailu <61507662+sailuj@users.noreply.github.com>
2022-02-17 12:05:50 +0530

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,12 +1,15 @@
-from flask import
-
-
-app = flask(__name__)
-
-@app.rout("/")
-def index():
-    retun "Hello, world!"
-
-
-if __name__ "__main__":
-    app.run()
+jobs:
+  build:
+    runs-on: ubuntu-20.04
+    strategy:
+      matrix:
+        java: [ '8', '11', '13', '15' ]
+    name: Java ${{ matrix.Java }} sample
+    steps:
+      - uses: actions/checkout@v2
+      - name: Setup java
+        uses: actions/setup-java@v2
+        with:
+          distribution: '<distribution>'
+          java-version: ${{ matrix.java }}
+      - run: java -cp java HelloWorldApp
